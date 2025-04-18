e35e1a463b76f4c539df35a3b2a9084db5b976aa
Pranjal Melkani <pranjal.melkani18@gmail.com>
2025-04-18 18:44:19 +0530

    api client with get/post methods added

---
+++ src/api_client/client.py
@@ -0,0 +1,36 @@
+import requests
+from ..config.settings import ConfigManager
+
+class ApiClient:
+    def __init__(self):
+        self.config = ConfigManager()
+        
+    def get(self, endpoint, query_parameters=None):
+        url = f"{self.config.base_url}{endpoint}"
+        try:
+            response = requests.get(
+                url,
+                headers = self.config.headers,
+                params = query_parameters,
+                timeout = self.config.timeout
+            )
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            raise Exception(f"GET request failed: {str(e)}")
+        
+    def post(self, endpoint, data=None, json=None, query_parameters=None):
+        url = f"{self.config.base_url}{endpoint}"
+        try:
+            response = requests.post(
+                url = url,
+                headers = self.config.headers,
+                data = data,
+                json = json,
+                params = query_parameters,
+                timeout = self.config.timeout
+            )
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            raise Exception(f"POST request failed: {str(e)}")
