b793d454166e1dfac95979b3b4b5752e14eb0450
Pranjal Melkani <pranjal.melkani18@gmail.com>
2025-04-19 11:01:31 +0530

    PUT method added

--- src/api_client/client.py
+++ src/api_client/client.py
@@ -34,3 +34,19 @@ class ApiClient:
             return response.json()
         except Exception as e:
             raise Exception(f"POST request failed: {str(e)}")
+        
+    def put(self, endpoint, data=None, json=None, query_parameters=None):
+        url = f"{self.config.base_url}{endpoint}"
+        try:
+            response = requests.put(
+                url = url,
+                data = data,
+                headers = self.config.headers,
+                params = query_parameters,
+                json = json,
+                timeout = self.config.timeout
+            )
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            raise Exception(f"PUT request failed: {str(e)}")
