73bec38c5e9df166d93eb518869901cd44a9cfe6
Pranjal Melkani <pranjal.melkani18@gmail.com>
2025-04-19 22:28:28 +0530

    updated assertion error messages

--- tests/test_brands.py
+++ tests/test_brands.py
@@ -9,8 +9,8 @@ class TestBrands:
         responseCode = response['responseCode']
         brands = response['brands']
         
-        assert responseCode == 200
-        assert len(brands) >= 1
+        assert responseCode == 200, "Expected response code: 200"
+        assert len(brands) >= 1, "No brand found"
         validate(response, get_all_brands_list_schema)
         for brand in brands:
             validate(brand, brand_schema)
@@ -21,6 +21,7 @@ class TestBrands:
         responseCode = response['responseCode']
         message = response['message']
         
-        assert responseCode == 405
-        assert str(message).lower().__contains__("this request method is not supported")
+        assert responseCode == 405, "Expected response code: 405"
+        assert str(message).lower().__contains__("this request method is not supported"), \
+            "Expected 'This request method is not supported' message"
         validate(response, put_all_brands_schema)
