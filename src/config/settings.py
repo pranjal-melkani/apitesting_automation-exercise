ad6aaa284d61e4df82d5fd685c5e55be03d56d99
Pranjal Melkani <pranjal.melkani18@gmail.com>
2025-04-19 11:46:43 +0530

    removed predefined headers

--- src/config/settings.py
+++ src/config/settings.py
@@ -2,10 +2,7 @@ class ConfigManager:
     def __init__(self):
         self.base_url = "https://automationexercise.com/api"
         self.timeout = 30
-        self.headers = {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
+        self.headers = {}
         
     def update_headers(self, headers):
         self.headers.update(headers)
