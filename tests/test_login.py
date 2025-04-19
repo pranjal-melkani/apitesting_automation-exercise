cbe684b0d2863bf89bb673fd7143f02ae8a43d12
Pranjal Melkani <pranjal.melkani18@gmail.com>
2025-04-19 12:59:49 +0530

    delete to verify login test added

--- tests/test_login.py
+++ tests/test_login.py
@@ -30,3 +30,13 @@ class TestLogin:
         assert str(message).lower().__contains__("email or password parameter is missing")
         validate(response, post_login_without_email_schema)
         
+    def test_delete_to_verify_login(self):
+        api = ApiClient()
+        response = api.delete("/verifyLogin")
+        responseCode = response['responseCode']
+        message = response['message']
+        
+        assert responseCode == 405
+        assert str(message).lower().__contains__("this request method is not supported")
+        validate(response, delete_to_verify_login_schema)
+        
